72090e23abfc0938df1150539b6e557b51b74d76
Nafsieh-Tofighi <nafiseh.tofighi@gmail.com>
2023-08-05 12:33:33 +0300

    003 material

---
+++ 003-Indention-Python_In_A_Minute.py
@@ -0,0 +1,13 @@
+def print_numbers():
+    for i in range(1, 6):
+        print(i)
+        if i == 3:
+            print("Three!")
+    print("Done!")
+
+
+num_list=[
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9]		
+	]
