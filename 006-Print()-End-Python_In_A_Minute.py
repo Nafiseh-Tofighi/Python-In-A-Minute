63ee8ea28432d0514133b1963b2aca1fadfec578
Nafiseh-Tofighi <nafiseh.tofighi@gmail.com>
2024-05-10 10:35:34 +0300

    006 added

---
+++ 006-Print()-End-Python_In_A_Minute.py
@@ -0,0 +1,21 @@
+"""
+File: 005-Print()-Separator-Python_In_A_Minute.py
+Author: Nafiseh J.Tofighi
+Date: 10May2024
+
+Description:
+This script demonstrates the usage of the 'sep' parameter in the print() function to control the separator between printed arguments.
+
+Usage:
+- Run the script to see examples of different separators in action.
+"""
+
+print('a','b','c')
+#output: a b c
+
+print('a','b','c', sep='')
+#output: abc
+
+
+print('a','b','c', sep='-')
+#output: a-b-c
