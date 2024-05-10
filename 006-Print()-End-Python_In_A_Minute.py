fdfeb0516588d10070b43115c4a39dd1c0528de7
Nafiseh-Tofighi <nafiseh.tofighi@gmail.com>
2024-05-10 10:54:36 +0300

    006 modified

--- 006-Print()-End-Python_In_A_Minute.py
+++ 006-Print()-End-Python_In_A_Minute.py
@@ -1,21 +1,54 @@
 """
-File: 005-Print()-Separator-Python_In_A_Minute.py
+File: 006-Print()-End-Python_In_A_Minute.py
 Author: Nafiseh J.Tofighi
 Date: 10May2024
 
 Description:
-This script demonstrates the usage of the 'sep' parameter in the print() function to control the separator between printed arguments.
+This script demonstrates the usage of the 'end' parameter in the print() function to control the behavior of the end of the printed output.
 
 Usage:
-- Run the script to see examples of different separators in action.
+- Run the script to see examples of different 'end' parameter usages.
 """
 
-print('a','b','c')
-#output: a b c
+print('a')
+print('b')
+print('c')
 
-print('a','b','c', sep='')
+#output: a
+#b
+#c
+
+print('a', end='')
+print('b', end='')
+print('c', end='')
 #output: abc
 
+print('Hello', end='!')
+print('World', end='!')
+#output: Hello!World!
 
-print('a','b','c', sep='-')
-#output: a-b-c
+def draw_tree(levels):
+    for i in range(1, levels + 1):
+        print(" " * (levels - i), end="")
+        print("*" * (2*i - 1), end="\n")
+
+    # Print the trunk
+    trunk_width = levels // 2
+    trunk_spaces = (2*levels - trunk_width) // 2
+    for _ in range(trunk_width):
+        print(" " * trunk_spaces, end="")
+        print("*" * trunk_width, end="\n")
+
+draw_tree(6)
+#output:
+
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+# ***********
+#     ***
+#     ***
+#     ***
+
